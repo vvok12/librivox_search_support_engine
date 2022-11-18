3d8bfe2a3b0d4d2d65643eea128c4c788bf1c841
vvok12 <v.v.bezshtanko@gmail.com>
2022-11-18 05:04:17 +0200

    generalizing algo

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,30 @@
 mod libgen_dump;
 mod sql_conversion;
 
-use std::fs::File;
-use std::io::prelude::*;
-
 use libgen_dump::LibGenDump;
 use sql_conversion::FictionSql;
 
 fn main() {
     
     libgen_fiction_strategy();
-    //println!("dump is {:?}", dump.dump);
 }
 
 
 fn libgen_fiction_strategy() {
-    let dump = LibGenDump::load();
-    let fiction_sql = dump.extract();
-    
-    let fiction = FictionSql(fiction_sql);
-    let fiction_rows:Vec<String> = fiction.insert_rows_scripts().take(1).map(|r| r.unwrap()).collect();
-    let _fiction_tables = fiction.tables_script();
+    let fiction_sql_file = LibGenDump::load().extract();
+    upload(FictionSql(fiction_sql_file));
+}
 
-    _ = File::create("fiction/fiction_rows.txt").unwrap().write(fiction_rows[0].as_bytes());
+fn upload(_fiction_sql: FictionSql) {
+    todo!()
 }
 
 #[test]
 fn select_sql() {
+    use std::fs::File;
+    use std::io::prelude::*;
+    
+
     let fiction_sql = File::open("fiction/fiction.sql").unwrap();
 
     let fiction = FictionSql(fiction_sql);
